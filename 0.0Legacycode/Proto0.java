43e5fe3d7b1c57b485c4b573cdf04d270c36f694
RenardFake <rmacorol18@gmail.com>
2023-10-14 07:56:54 +0800

    Refactoring 0.0Legacycode

--- 0.0Legacycode/Proto0.java
+++ 0.0Legacycode/Proto0.java
@@ -2,21 +2,40 @@ import java.util.*;
 public class Proto0{
     
     public static void main(String[] args) {
+      startText();
+    }
+
+    static void startText(){
         Scanner s = new Scanner(System.in);
-        Random r = new Random();
-       
-        String array[] = {"School", "Personal Project", "Work"};
+        
         System.out.println("Good day Welcome to my Project");
         System.out.println();
         System.out.println("Type 'Y' to Proceed" );
         String input=s.nextLine();
+        execute(input);
+    }
+
+    static void execute(String input){
+        final String array[] = {"School", "Personal Project", "Work"};
+        Random r = new Random();
+
         if(input.equalsIgnoreCase("y")){
             int randomize =r.nextInt(array.length);
+            System.out.println();
             System.out.println("The things that you will do are:");
             System.out.println(array[randomize]);
-
+            System.out.println();
+        }else{
+            System.out.println();
+            System.out.println("your input are invalid ");
+            System.out.println();
         }
+        credit();
+    }
 
-
+    static void credit(){
+        System.out.println("Created By: Wonkabars");
+        System.out.println("Github: https://github.com/RenardFake");
+        System.out.println("Facebook: https://www.facebook.com/profile.php?id=100009456221522 ");
     }
 }
